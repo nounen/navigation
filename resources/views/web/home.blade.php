839fb1b981a17d0d6f2d92303945a96fe2d8318f
lazyou <1394139539@qq.com>
2018-03-26 01:06:46 +0800

    幻灯片自动播放设置

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -144,6 +144,9 @@
     <script>
     var swiper = new Swiper('.swiper-container', {
         loop: true,
+        autoplay: {
+            delay: 3000,
+        },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
