7813391cb50eb04c9a5c85905eb5dfceeea24cbc
nounen <281580047@qq.com>
2018-03-25 16:31:02 +0800

    fix: 重定向错误

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(url('admin'));
+            return redirect(url('admin/setting/edit'));
         }
 
         return $next($request);
