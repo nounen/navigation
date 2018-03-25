d2f6beaf86bb3ba820d199527a6594cecffd2a61
nounen <281580047@qq.com>
2018-03-25 15:14:15 +0800

    路由: 首页正则匹配情况

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 Route::namespace('Web')->name('web.')->group(function () {
     Route::get('/', 'HomeController@home');
-    Route::get('/{id}', 'HomeController@home');
+    Route::get('/{id}', 'HomeController@home')->where('id', '[0-9]+');
 });
